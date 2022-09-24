2faa4de0e2402d758bf620e27e964fbfbd605744
Yurik <belozerov.yurik@yandex.ru>
2022-09-24 10:37:55 +0300

    add 2 files

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Hellow world');
+const myPlanet = 'Земля';
+console.log(myPlanet)
+let timeNow = '';
+let amountPage = 5;
+const isCash = true;
+const firstName = 'Б';
+const secondName = 'Ю';
+const surName = 'A';
+console.log(timeNow, amountPage, isCash)
